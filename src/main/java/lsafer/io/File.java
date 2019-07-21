82d24efa4d6a26bceb49e79f189a75a95e4d702e
LSaferSE <lsaferse@gmail.com>
2019-07-21 21:49:25 +0300

    end test

--- src/main/java/lsafer/io/File.java
+++ src/main/java/lsafer/io/File.java
@@ -37,7 +37,6 @@ public class File extends java.io.File {
      * needs to replace the hole object
      * to effect results
      * such as renaming or moving the file.
-     * xxxx
      */
     public File self = this;
 
