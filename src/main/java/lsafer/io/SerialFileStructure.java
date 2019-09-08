2e9c961e652127a696a6bda3c01cc061ae25bb10
LSaferSE <lsaferse@gmail.com>
2019-09-08 08:03:57 +0300

    update on SerializableFileStructue.class
    refactor -> SerializableFileStructure -> SerialFileStructure

--- src/main/java/lsafer/io/SerialFileStructure.java
+++ src/main/java/lsafer/io/SerialFileStructure.java
@@ -7,7 +7,7 @@ import java.io.Serializable;
  * Depends on {@link File#readSerializable(Class, java.util.function.Supplier)} and {@link File#writeSerializable(Serializable)}.
  *
  * <ul>
- * <li>note: make sure your {@link SerializableFileStructure serial-file-structure} matches all {@link FileStructure file-structures} rules.</li>
+ * <li>note: make sure your {@link SerialFileStructure serial-file-structure} matches all {@link FileStructure file-structures} rules.</li>
  * </ul>
  *
  * @author LSaferSE
@@ -15,10 +15,10 @@ import java.io.Serializable;
  * @see java.io.Serializable
  * @since 13-Jul-19
  */
-public class SerializableFileStructure extends FileStructure {
+public class SerialFileStructure extends FileStructure {
     @Override
     public <F extends FileStructure> F load() {
-        this.putAll(this.remote.readSerializable(SerializableFileStructure.class, SerializableFileStructure::new));
+        this.putAll(this.remote.readSerializable(SerialFileStructure.class, SerialFileStructure::new));
         return (F) this;
     }
 
